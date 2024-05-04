b99aa8d5c00f559e3037fc451a12e5a33200b0b3
Lopany <pityu1617@gmail.com>
2024-05-04 10:29:12 +0200

    save

---
+++ cmake-build-debug/CMakeFiles/BME_prog2_tickets.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BME_prog2_tickets.exe"
+  "BME_prog2_tickets.exe.manifest"
+  "BME_prog2_tickets.pdb"
+  "CMakeFiles/BME_prog2_tickets.dir/main.cpp.obj"
+  "CMakeFiles/BME_prog2_tickets.dir/main.cpp.obj.d"
+  "libBME_prog2_tickets.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BME_prog2_tickets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
